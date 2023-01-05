c1c3f3723c419c2120209b7243883e655e9c8a59
TwanstDeveloper <twanadizain5@gmail.com>
2023-01-05 15:46:28 +0300

    Php Tutorial

--- Test/inddex.php
+++ Test/inddex.php
@@ -1,40 +1,13 @@
 <?php
-// Loop In Php
 
-$arr=["Twanst "," Codes "," Error "," Php"];
+$password="97";
 
-//for loop
-echo"This is Statment For Loop In Php <br>";
-for($i=0;$i<count($arr);$i++){
-    echo $arr[$i];
-}
+if($password=="97"){
+    echo"You are logged !";
 
-echo"<hr>";
-//foreach Loop
-echo"This is Statment Foreach Loop In Php <br>";
+}else{
+    echo "Please Try Again !";
 
-foreach($arr as $x){
-    echo $x;
-}
-echo"<hr>";
-//while Loop
-echo"This is Statment while Loop In Php <br>";
-$f=0;
-while($f<count($arr)){
-    echo $arr[$f] ;
-    $f++;
-}
-echo"<hr>";
-//Do while Loop
-echo"This is Statment Do while Loop In Php <br>";
-$m=0;
-do{
-echo $arr[$m];
-$m++;
-}while($m<count($arr))
-
-
-
-
+} 
 
 ?>
