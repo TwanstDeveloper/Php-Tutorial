efd016e924f6f24075de63a6dc5a5a815a61f22c
TwanstDeveloper <twanadizain5@gmail.com>
2023-01-05 21:00:27 +0300

    Hello

--- Test/inddex.php
+++ Test/inddex.php
@@ -1,3 +1,28 @@
 <?php
 include 'nav.php';
 ?>
+<?php
+$error=['result'=>''];
+if(isset($_POST['btn'])){
+
+   $us=htmlspecialchars($_POST['u']);
+   $pass=htmlspecialchars($_POST['p']);
+   if(empty($us) || empty($pass)){
+    $error['result']="Tkaia xanakan Prbkawa";
+   }
+ elseif (!filter_var($us, FILTER_VALIDATE_EMAIL)) {
+    $error['result'] = "Invalid email format";
+}
+else{
+    echo "success";
+}
+
+}
+
+?>
+<form action="inddex.php" method="post">
+    <p class="alert alert-white text-danger"><?php $error=['result'];?></p>
+        <input type="email" placeholder="userame" name="u" ><br>
+        <input type="pass" placeholder="Password" name="p"><BR>
+        <button name="btn">Login</button>
+    </form>
