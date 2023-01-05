732a9b5cbc09dc92e07ba37df2314de0ace412ec
TwanstDeveloper <twanadizain5@gmail.com>
2023-01-05 16:49:00 +0300

    Hello Im Twanst Codes

--- Test/inddex.php
+++ Test/inddex.php
@@ -19,9 +19,59 @@ $TXT="<b> PHP !</b>";
 
 echo "Im Love Learn ".$TXT." Easy<br>";
 
+echo"<hr>";
+//Idea Conditioanl In Php
 
+// write programing in php for conditional date time now Day or night
 
+// first just Day
+$t=date("H");
+if($t<"20"){
+    echo "Good Day";
+}
+//both
+/* 
+$t=date("H");
+if($t<"20"){
+    echo "Good Day";
+}else{
+    echo"Good Night";
+}
+*/
+echo"<hr>";
+//Write Programm drive age 
+$age=18;
+if($age>=18){
+    echo"Successfully Age For Driving";
+}else{
+    echo"Please try again For Age 18 or more";
+}
+echo"<hr>";
 
+// write programing in php For Odd and even number
+$num1=100;
 
+if($num1 %2==0){
+    echo"Even";
+}else{
+    echo"Odd";
+}
+echo"<hr>";
+// write programiing in php Max number Bettwin Three Number
+
+$x=2;
+$f=23;
+$r=4;
+
+if($x>$f || $x>$r){
+    echo "value X Max Num Ber It Is:".$x;
+}elseif($f>$x || $f>$r){
+    echo"Value F Max Num It Is:".$f;
+}elseif($r>$x || $r>$f){
+    echo"Value R Max Num It Is:".$r;
+}else{
+    echo "All Number Equail";
+}
+echo"<hr>";
 
 ?>
